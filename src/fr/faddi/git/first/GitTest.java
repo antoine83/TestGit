9bde86d08cfc7fd498d8782f4d6411745fe58a40
Antoine Faddi <antoine.linux@sfr.fr>
2012-03-04 22:49:47 +0100

    Creation

---
+++ src/fr/faddi/git/first/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.faddi.git.first;
+
+/**
+ * @author Antoine
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+
+	}
+
+}
